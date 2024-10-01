d6ed8bff25e6ac763bdedd8cc61698177a53fbe9
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-10-01 14:18:42 -0400

    Update "About Me" and implement javascript
    I revised my entire About Me section to be a little more engaging, and
    less meandering (I'd originally wrote it just for my own reference). I
    also started to implement the javascript I'd been playing with. Now,
    the page is blank to start, and clicking a link in the nav bar "opens"
    the appropriate page, so that I can have multiple pages without
    refreshing the entire screen. I am sure there are "better" ways to do
    this, but I am still learning.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const linksText = document.querySelector(".links-text");
 const linksTag = document.querySelector("#links-tag");
 
-linksTag.addEventListener('click', () => linksText.innerHTML = 
-'<object width="100%" type="text/html" data="links.html"</object>')
+const bodyText = document.querySelector(".article");
+const aboutLink = document.querySelector("#about-link");
+
+
+aboutLink.addEventListener('click', () => {
+    bodyText.innerHTML = 
+'<object width="100%" type="text/html" data="about.html"</object>'
+})
+
+linksTag.addEventListener('click', () => {
+    bodyText.innerHTML = 
+'<object width="100%" type="text/html" data="links.html"</object>'
+})
 
