9c14db4caaa3ab0ada328cd3ac1d441b50634b26
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-10-02 12:03:47 -0400

    Add all pages, remove javascript
    It's clear that I don't yet know enough javascript to get this site to
    do what I want it to do, so I have scrapped that idea for now, and
    turned this into a static site, as I feel it is more important to get
    it published first and modify it later.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const linksText = document.querySelector(".links-text");
 const linksTag = document.querySelector("#links-tag");
 
-const bodyText = document.querySelector(".article");
+const bodyText = document.querySelector(".body-text");
 const aboutLink = document.querySelector("#about-link");
 
 
-aboutLink.addEventListener('click', () => {
-    bodyText.innerHTML = 
-'<object width="100%" type="text/html" data="about.html"</object>'
-})
+// aboutLink.addEventListener('click', () => {
+//     bodyText.innerHTML = 
+// '<object width="100%" type="text/html" data="about.html"</object>'
+// })
+
+async function getText(file) {
+    let myObject = await fetch(file);
+    let myText = await myObject.text();
+    bodyText.innerHTML = myText;
+}
+
+// let file = "about.html"
+aboutLink.addEventListener('click', getText("about.html"))
 
 linksTag.addEventListener('click', () => {
     bodyText.innerHTML = 
