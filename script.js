9aa713f577919448c7cea3ad4e2172a7bdb4e5c1
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-10-07 20:07:25 -0400

    Add JS to load the nav menu to each page
    Instead of having the UL on each page, I added some JavaScript to add
    the list automatically to each page, so that I don't have to change
    each page every time I change something. I also added a bit of code
    to the javascript to highlight the current page in the list.

---
+++ script.js
@@ -0,0 +1,36 @@
+const navList = document.querySelector('.nav');
+const img = document.querySelector(".nav img");
+let list = document.createElement('ul');
+
+let listItem1 = document.createElement('li');
+listItem1.id = "index";
+listItem1.innerHTML = '<a href="./index.html">I.&nbsp;&nbsp;&nbsp;About Me</a>';
+
+let listItem2 = document.createElement('li');
+listItem2.id = "links";
+listItem2.innerHTML = '<a href="./links.html">II.&nbsp;&nbsp;Projects</a>';
+
+let listItem3 = document.createElement('li');
+listItem3.id = "contact";
+listItem3.innerHTML = '<a href="./contact.html">III.&nbsp;Contact</a>';
+
+let listItem4 = document.createElement('li');
+listItem4.id = "blog";
+listItem4.innerHTML = '<a href="./blog.html">IV.&nbsp;&nbsp;Blog</a>';
+
+let listItem5 = document.createElement('li');
+listItem5.id = "pageinfo";
+listItem5.innerHTML = '<a href="./pageinfo.html">V.&nbsp;&nbsp;&nbsp;Page Info</a>';
+
+navList.insertBefore(list, img);
+list.append(listItem1);
+list.append(listItem2);
+list.append(listItem3);
+list.append(listItem4);
+list.append(listItem5);
+
+const page = document.URL;
+let pageID = '#' + 
+        page.slice(page.lastIndexOf('/')+1,page.lastIndexOf('.'));
+const currentPage = document.querySelector(pageID);
+currentPage.classList.toggle("highlight");
